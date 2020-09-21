f80851ed88effba340d2fbe78044bcf9687dde5b
conrad_buys <conradbuys11@gmail.com>
2020-09-21 14:17:57 -0400

    Work on cocktails controller.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,28 +1,44 @@
 class CocktailsController < ApplicationController
 
+    before_action :find_cocktail, only [:show, :edit, :update, :destroy] #makes it so at the beginning of all four of these methods, find_cocktail is ran (see private method)
+
     def index
+        @cocktails = Cocktail.all
     end
     
     def show
+        #find_cocktail does the work for us - don't have to write any code here!
     end
 
     def edit
     end
     
     def update
+        @cocktail.update(cocktail_params) #see our private method
+        redirect_to(cocktail_path(@cocktail))
     end
 
     def new
+        @cocktail = Cocktail.new
     end
 
     def create
+        cocktail = Cocktail.create(cocktail_params) #see our private method
+        redirect_to(cocktail_path(cocktail))
     end
     
     def destroy
+        @cocktail.destroy
+        redirect_to(cocktails_path)
     end
 
     private 
 
-    def bar_params
+    def cocktail_params
+        params.require(:cocktail).permit(:name, :price, :description)
+    end
+
+    def find_cocktail
+        @cocktail = Cocktail.find(params[:id])
     end
 end
