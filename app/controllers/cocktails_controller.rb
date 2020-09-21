312d707142a7b901ca7f5aa98d549c9b709c876d
JBondeson19 <jpbond58@gmail.com>
2020-09-21 12:58:30 -0400

    "cocktails controller"

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,28 @@
 class CocktailsController < ApplicationController
+
+    def index
+    end
+    
+    def show
+    end
+
+    def edit
+    end
+    
+    def update
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
+    def destroy
+    end
+
+    private 
+
+    def bar_params
+    end
 end
