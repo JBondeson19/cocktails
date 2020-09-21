672115b4e7388e4868c1ad2e7ae76582f66abcf5
JBondeson19 <jpbond58@gmail.com>
2020-09-21 12:58:15 -0400

    "bars controller"

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,2 +1,39 @@
 class BarsController < ApplicationController
+    
+    before_action :find_bar, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @bars = Bar.all
+    end
+    
+    def show
+    end
+
+    def edit
+    end
+    
+    def update
+        @bar.update(name: params[:bar][:name],
+                    location: params[:bar][:location])
+        redirect_to bars_path(bars)
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
+    def destroy
+    end
+
+    private 
+
+    def find_bar
+        @bar = Bar.find(params[:id])
+    end
+
+    def bar_params
+
+    end
 end
